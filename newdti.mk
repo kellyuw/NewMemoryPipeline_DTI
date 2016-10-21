f0fbd088bcc9553221e6a98dc24fb91b89d5affc
kellyuw <kelly89@uw.edu>
2016-10-21 11:59:58 -0700

    Updated makefile to included skullstrip before eddy

--- newdti.mk
+++ newdti.mk
@@ -1,9 +1,5 @@
 
-# This makefile runs through the DTI preprocessing pipleline
-# Warning: non-standard configuration as below:
-# 1. Left-Right phase encoding direction
-# 2. Skull-stripping after tensor fit
-# 3. Very dilated (2x) brain mask (due to Left-Right phase encoding direction issue)
+# This makefile runs through the DTI preprocessing pipeline
 
 cwd = $(shell pwd)
 OUTDIR=out
@@ -15,7 +11,7 @@ SDC_METHOD = $(shell if [ -f fieldmap_phase.nii.gz ] ; then echo FUGUE; \
 # Set open MP number of threads to be 1, so that we can parallelize using make.
 export OMP_NUM_THREADS=1
 
-.PHONY: Convert BrainMasks Eddy FitTensor DTIQA TestMasks RegMasks RegDTI
+.PHONY: Convert BrainMasks Eddy FitTensor DTIQA RegMasks RegDTI
 
 # keep everything by default
 .SECONDARY:
@@ -32,14 +28,15 @@ define dti_usage
 endef
 
 Convert: fieldmap/B0.nii.gz dti/DTI64.nii.gz
-BrainMasks: dti/S0.nii.gz dti/S0_fake_mask.nii.gz
+BrainMasks: dti/S0.nii.gz memprage/T1_optiBET_brain_mask_dilx2.nii.gz memprage/T1_brain_masked_dilx2.nii.gz xfm_dir/T1_brain_mask_to_S0_a_dilx2_Warped.nii.gz xfm_dir/T1_brain_mask_to_S0_r_dilx2_Warped.nii.gz dti/brainmask.nii.gz dti/brainmask_dilx2.nii.gz
 Eddy: dti/acqparams.txt dti/mc_dti/mc_DTI64.nii.gz
 FitTensor: dti/dtifit/dti_FA.nii.gz
 DTIQA: QA/DTI64_QASummary.txt QA/QA_Metrics.txt
 TestMasks: $(patsubst %,memprage/T1_optiBET_brain_mask_dilx%.nii.gz, 0 1 2 3) $(patsubst %,xfm_dir/T1_brain_mask_to_FA_r_dilx%_Warped.nii.gz, 0 1 2 3)
-RegMasks: dti/brainmask.nii.gz dti/dtifit/dti_FA_brain.nii.gz xfm_dir/dti_FA_brain_to_MNI_r_Warped.nii.gz
+RegMasks: dti/dtifit/dti_FA_brain.nii.gz xfm_dir/dti_FA_brain_to_MNI_r_Warped.nii.gz
 RegDTI: xfm_dir/dti_FA_to_MNI_Warped.nii.gz
 PreprocessSubject: Convert BrainMasks Eddy FitTensor DTIQA RegMasks RegDTI
+
 
 ## 1. Convert PAR/RECs to NIFTI
 fieldmap/B0.nii.gz: ${SubjDir}/parrecs/B0.PAR ${SubjDir}/parrecs/B0.REC
@@ -52,56 +49,49 @@ dti/DTI64.nii.gz: ${SubjDir}/parrecs/DTI64.PAR ${SubjDir}/parrecs/DTI64.REC
 dti/S0.nii.gz: dti/DTI64.nii.gz
 	fslroi $(word 1,$^) $@ 0 1 ;\
 
-dti/S0_fake_mask.nii.gz: dti/S0.nii.gz
-	cp $(word 1,$^) $@ ;\
+memprage/T1_optiBET_brain_mask_dilx2.nii.gz: memprage/T1_optiBET_brain_mask.nii.gz
+	fslmaths $(word 1,$^) -dilM -dilM -bin $@ ;\
 
+memprage/T1_brain_masked_dilx2.nii.gz: memprage/T1_optiBET_brain_mask_dilx2.nii.gz memprage/T1.nii.gz
+	fslmaths $(word 2,$^) -mas $(word 1,$^) $@
+
+xfm_dir/T1_brain_mask_to_S0_r_dilx2_Warped.nii.gz: memprage/T1_brain_masked_dilx2.nii.gz dti/S0.nii.gz
+	SGE_RREQ="-q global.q" antsRegistrationSyN.sh -d 3 -t r -f dti/S0.nii.gz -m memprage/T1_brain_masked_dilx2.nii.gz -o xfm_dir/T1_brain_mask_to_S0_r_dilx2_ ;\
+
+xfm_dir/T1_brain_mask_to_S0_a_dilx2_Warped.nii.gz: memprage/T1_brain_masked_dilx2.nii.gz dti/S0.nii.gz
+	SGE_RREQ="-q global.q" antsRegistrationSyN.sh -d 3 -t a -f dti/S0.nii.gz -m memprage/T1_brain_masked_dilx2.nii.gz -o xfm_dir/T1_brain_mask_to_S0_a_dilx2_ ;\
+
+dti/brainmask.nii.gz: xfm_dir/T1_brain_mask_to_S0_a_dilx2_Warped.nii.gz
+	fslmaths $(word 1,$^) -bin $@ ;\
+
+dti/brainmask_dilx2.nii.gz: dti/brainmask.nii.gz
+	fslmaths $(word 1,$^) -dilM -dilM $@ ;\
 
 ## 3. Do motion correction with eddy
 dti/acqparams.txt: dti/DTI64.nii.gz
 	dwell_time=`cat dti/DTI64.dwell_time` ;\
 	echo "1 0 0 $${dwell_time}" > $@ ;\
 
-dti/mc_dti/mc_DTI64.nii.gz: dti/DTI64.nii.gz dti/S0_fake_mask.nii.gz dti/acqparams.txt
-		mkdir -p `dirname $@` ;\
-		bash /mnt/stressdevlab/scripts/DTI/dti_preproc/motion_correct.sh -a $(SubjDir)/dti/acqparams.txt -k $(SubjDir)/dti/DTI64.nii.gz -b $(SubjDir)/dti/DTI64.bvals -r $(SubjDir)/dti/DTI64.bvecs -M $(SubjDir)/dti/S0_fake_mask.nii.gz -o $(SubjDir)/`dirname $@` ;\
+dti/mc_dti/mc_DTI64.nii.gz: dti/DTI64.nii.gz dti/brainmask.nii.gz dti/acqparams.txt
+	mkdir -p `dirname $@` ;\
+	SGE_RREQ="-q global.q" bash /mnt/stressdevlab/scripts/DTI/dti_preproc/motion_correct.sh -a $(SubjDir)/dti/acqparams.txt -k $(SubjDir)/dti/DTI64.nii.gz -b $(SubjDir)/dti/DTI64.bvals -r $(SubjDir)/dti/DTI64.bvecs -M dti/brainmask.nii.gz -o $(SubjDir)/`dirname $@` -p $(SubjDir)/parrecs/DTI64.PAR ;\
+
 
 ## 4. Fit tensors with dtifit
-dti/dtifit/dti_FA.nii.gz: dti/mc_dti/mc_DTI64.nii.gz
-		/mnt/stressdevlab/scripts/DTI/dti_preproc/ols_fit_tensor.sh -k $(word 1,$^) -b dti/DTI64.bvals -r dti/mc_dti/bvec_mc.txt -M dti/S0_fake_mask.nii.gz -o $(SubjDir)/`dirname $@` -f ;\
+dti/dtifit/dti_FA.nii.gz: dti/mc_dti/mc_DTI64.nii.gz dti/brainmask.nii.gz
+	/mnt/stressdevlab/scripts/DTI/dti_preproc/ols_fit_tensor.sh -k $(word 1,$^) -b dti/DTI64.bvals -r dti/mc_dti/bvec_mc.txt -M dti/brainmask.nii.gz -o $(SubjDir)/`dirname $@` -f ;\
+
 
 ## 5. Brain mask
 
-xfm_dir/T1_brain_mask_to_FA_r_dilx%_Warped.nii.gz: dti/dtifit/dti_FA.nii.gz memprage/T1_optiBET_brain_mask_dilx%.nii.gz
-	dilval=$* ;\
-	antsRegistrationSyN.sh -d 3 -t r -f dti/dtifit/dti_FA.nii.gz -m $(word 2,$^) -o xfm_dir/T1_brain_mask_to_FA_r_dilx$${dilval}_ ;\
-
-memprage/T1_optiBET_brain_mask_dilx0.nii.gz: memprage/T1_optiBET_brain_mask.nii.gz
-	fslmaths $(word 1,$^) -bin $@ ;\
-
-memprage/T1_optiBET_brain_mask_dilx1.nii.gz: memprage/T1_optiBET_brain_mask.nii.gz
-	fslmaths $(word 1,$^) -dilM -bin $@ ;\
-
-memprage/T1_optiBET_brain_mask_dilx2.nii.gz: memprage/T1_optiBET_brain_mask.nii.gz
-	fslmaths $(word 1,$^) -dilM -dilM -bin $@ ;\
-
-memprage/T1_optiBET_brain_mask_dilx3.nii.gz: memprage/T1_optiBET_brain_mask.nii.gz
-	fslmaths $(word 1,$^) -dilM -dilM -dilM -bin $@ ;\
-
-memprage/T1_brain_masked_dilx%.nii.gz: memprage/T1_optiBET_brain_mask_dilx%.nii.gz memprage/T1.nii.gz
-	fslmaths $(word 2,$^) -mas $(word 1,$^) $@
 
 dti/mc_dti/mc_DTI64_S0.nii.gz: dti/mc_dti/mc_DTI64.nii.gz
 	fslroi $(word 1,$^) $@ 0 1 ;\
 
-dti/brainmask.nii.gz: xfm_dir/T1_brain_mask_to_FA_r_dilx2_Warped.nii.gz
-	fslmaths xfm_dir/T1_brain_mask_to_FA_r_dilx2_Warped.nii.gz -bin $@ ;\
-
-dti/dtifit/dti_FA_brain.nii.gz: dti/brainmask.nii.gz dti/dtifit/dti_FA.nii.gz 
-	fslmaths dti/dtifit/dti_FA.nii.gz -mas $(word 1,$^) $@ ;\
 
 #This is important for template creation!
-xfm_dir/dti_FA_brain_to_MNI_r_Warped.nii.gz: dti/dtifit/dti_FA_brain.nii.gz
-	antsRegistrationSyN.sh -d 3 -n 20 -t r -f /usr/share/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz -m dti/dtifit/dti_FA_brain.nii.gz -o xfm_dir/dti_FA_brain_to_MNI_r_ ;\
+xfm_dir/dti_FA_to_MNI_1mm_r_Warped.nii.gz: dti/dtifit/dti_FA.nii.gz
+	SGE_RREQ="-q global.q" antsRegistrationSyN.sh -d 3 -t r -f /usr/share/fsl/data/standard/MNI152_T1_1mm_brain.nii.gz -m dti/dtifit/dti_FA.nii.gz -o xfm_dir/dti_FA_to_MNI_1mm_r_ ;\
 
 ## 5. Do QA
 QA/dtiprep/dwi_fixed_QCReport.txt: dti/DTI64.nii.gz
